ed82707cde0c678a965e954a0529a3a513595019
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2025-04-21 12:33:32 +0200

    change python_package to package_name

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -5,7 +5,7 @@
 """This is a sample python file for testing functions from the source code."""
 from __future__ import annotations
 
-from python_package.hello_world import hello_world
+from package_name.hello_world import hello_world
 
 
 def hello_test():
