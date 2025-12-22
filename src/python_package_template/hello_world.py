4ff3a833874f452bf69eba073f6d767710bf8d8f
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2025-12-22 18:09:28 +0100

    chore: Add docstring to the functions in `hello_world.py` (#9)

--- src/python_package_template/hello_world.py
+++ src/python_package_template/hello_world.py
@@ -1,18 +1,32 @@
+"""Hello World Module."""
+
 from __future__ import annotations
 
 
 def hello_world(i: int = 0) -> str:
-    """Doc String."""
+    """Hello World Function.
+
+    Args:
+        i: An integer input.
+
+    Returns:
+        A string output.
+    """
     print("hello world")
     return f"string-{i}"
 
 
 def good_night() -> str:
-    """Doc String."""
+    """Good Night Function.
+
+    Returns:
+        A string output.
+    """
     print("good night")
     return "string"
 
 
-def hello_goodbye():
+def hello_goodbye() -> None:
+    """Hello Goodbye Function."""
     hello_world(1)
     good_night()
