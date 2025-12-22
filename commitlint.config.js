281bd294dd99dc7f39a77c2a15a4c1716e87717d
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2025-12-22 09:33:50 +0100

    chore: Update `.pre-commit-config.yaml`

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+// commitlint.config.js
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  // Optional: make it stricter (recommended)
+  rules: {
+    "subject-case": [
+      2,
+      "always",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
